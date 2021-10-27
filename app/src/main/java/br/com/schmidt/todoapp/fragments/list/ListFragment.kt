7e04d88916a2a915f60d5bdcb601f546a1ccf3cf
Adriano <adrvschmidt@gmail.com>
2021-10-27 16:30:41 -0300

    Added StaggeredGridLayoutManager instead of LinearLayout

--- app/src/main/java/br/com/schmidt/todoapp/fragments/list/ListFragment.kt
+++ app/src/main/java/br/com/schmidt/todoapp/fragments/list/ListFragment.kt
@@ -8,9 +8,7 @@ import android.widget.Toast
 import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.recyclerview.widget.ItemTouchHelper
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.*
 import br.com.schmidt.todoapp.R
 import br.com.schmidt.todoapp.data.models.ToDoData
 import br.com.schmidt.todoapp.data.viewmodel.ToDoViewModel
@@ -46,7 +44,7 @@ class ListFragment : Fragment(), SearchView.OnQueryTextListener {
     private fun setAdapterView() {
         val recyclerView = binding.recyclerView
         recyclerView.adapter = adapter
-        recyclerView.layoutManager = LinearLayoutManager(requireActivity())
+        recyclerView.layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
         recyclerView.itemAnimator = SlideInUpAnimator().apply {
             addDuration = 300
         }
